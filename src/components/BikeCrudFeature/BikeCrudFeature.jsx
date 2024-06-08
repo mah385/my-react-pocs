2a6143621995f73f09c615790b16ec0d49c91800
mah385 <mdarifhussain385@gmail.com>
2024-06-09 00:57:50 +0530

    did code refactoring and rearranged imports

--- src/components/BikeCrudFeature/BikeCrudFeature.jsx
+++ src/components/BikeCrudFeature/BikeCrudFeature.jsx
@@ -1,10 +1,12 @@
 // import styles from "./BikeCrudFeature.module.css";
-
+/*-------------------------------------------------------------------*/
 import { useState } from "react";
-
+/*-------------------------------------------------------------------*/
 import UpdateBike from "./UpdateMobile/UpdateBike.jsx";
 import AddNewBike from "./AddNewBike/AddNewBike.jsx";
 import DisplayBike from "./DisplayBike/DisplayBike.jsx";
+
+/*-------------------------------------------------------------------*/
 
 function BikeCrudFeature() {
   const [showUpdateComponent, setShowUpdateComponent] = useState(false);
